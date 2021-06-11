22e8caaa3041e00b6fddfbc488eefe28b3e687a3
Jash <jashanmunder>
2021-06-11 21:21:40 +0200

    header and footer setup

---
+++ src/components/header/header.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import { Box, Icon } from '@material-ui/core';
+import AccountBalanceIcon from '@material-ui/icons/AccountBalance';
+
+export default function Header() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    
+    <Box display="flex" justifyContent="space-between" padding="20px">
+      <AccountBalanceIcon/>
+        
+        <div>
+      <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
+        Trend
+      </Button>
+      <Menu
+        id="simple-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <MenuItem onClick={handleClose}>Profile</MenuItem>
+        <MenuItem onClick={handleClose}>My account</MenuItem>
+        <MenuItem onClick={handleClose}>Logout</MenuItem>
+      </Menu>
+      <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
+        Categories
+      </Button>
+      <Menu
+        id="simple-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <MenuItem onClick={handleClose}>Profile</MenuItem>
+        <MenuItem onClick={handleClose}>My account</MenuItem>
+        <MenuItem onClick={handleClose}>Logout</MenuItem>
+      </Menu>
+      
+      <Button color="primary">Sign In</Button>
+      <Button color="primary">Get Started</Button> 
+      </div>
+
+    </Box>
+  );
+}

bcdb3b8722242709f6b41fe0bb20562eeaee6135
Plokamis Laptop <zorgthira4@hotmail.com>
2021-06-14 21:38:20 +0200

    register with auth api

--- src/components/register/register.jsx
+++ src/components/register/register.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
@@ -7,24 +7,13 @@ import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
 import Paper from '@material-ui/core/Paper';
-import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
+import {register} from '../../utils/auth-api';
 
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
+
 
 const useStyles = makeStyles((theme) => ({
   image: {
@@ -54,7 +43,24 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+
 export default function SignInSide() {
+
+const [email,setEmail]=useState("");
+const [password,setPassword]=useState("");
+
+async function handleSubmit(e){
+    e.preventDefault();
+}
+
+function handleEmailInput(e){
+    setEmail(e.target.value)
+}
+function handlePasswordInput(e){
+    setPassword(e.target.value)
+}
+
+
   const classes = useStyles();
 
   return (
@@ -69,7 +75,7 @@ export default function SignInSide() {
           <Typography component="h1" variant="h5">
             Get Started
           </Typography>
-          <form className={classes.form} noValidate>
+          <form className={classes.form} onSubmit={handleSubmit} noValidate>
             <TextField
               variant="outlined"
               margin="normal"
@@ -80,6 +86,8 @@ export default function SignInSide() {
               name="email"
               autoComplete="email"
               autoFocus
+              onChange={handleEmailInput}
+              value={email}
             />
             <TextField
               variant="outlined"
@@ -91,9 +99,12 @@ export default function SignInSide() {
               type="password"
               id="password"
               autoComplete="current-password"
+              onChange={handlePasswordInput}
+              value={password}
+              
             />
             <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
+              control={<Checkbox value="accept" color="primary" />}
               label="I accept privacy policy and terms of use"
             />
             <Button
---
+++ src/utils/auth-api.js
@@ -0,0 +1,22 @@
+
+export async function register({email, password, e}){
+    alert("Enter Register")
+    console.log(email)
+    console.log(password)
+    // Here we fetch against API
+
+    //FAIL CASE
+    if(email ===''){
+        alert("Error")
+        return {
+            message: 'Email is not valid, please try again',
+            status: 404
+        }
+    }
+    //CORRECT CASE
+    alert("Correct")
+    return {        
+        message: 'Register Completed',
+        status: 201
+    }
+}

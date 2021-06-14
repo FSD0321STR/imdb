db87d0bdd713d5adc1047da0d2692a29d13d88f1
Plokamis Laptop <zorgthira4@hotmail.com>
2021-06-14 19:57:23 +0200

    first view register, without function, without slideDown

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
-import Button from '@material-ui/core/Button';
-import Menu from '@material-ui/core/Menu';
-import MenuItem from '@material-ui/core/MenuItem';
-import { Box, Icon } from '@material-ui/core';
-import AccountBalanceIcon from '@material-ui/icons/AccountBalance';
+import React from "react";
+import Button from "@material-ui/core/Button";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+import { Box, Icon } from "@material-ui/core";
+import AccountBalanceIcon from "@material-ui/icons/AccountBalance";
+import Register from "../register/register"
 
 export default function Header() {
   const [anchorEl, setAnchorEl] = React.useState(null);
@@ -17,44 +18,53 @@ export default function Header() {
   };
 
   return (
-    
-    <Box display="flex" justifyContent="space-between" padding="20px">
-      <AccountBalanceIcon/>
-        
-        <div>
-      <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
-        Trend
-      </Button>
-      <Menu
-        id="simple-menu"
-        anchorEl={anchorEl}
-        keepMounted
-        open={Boolean(anchorEl)}
-        onClose={handleClose}
-      >
-        <MenuItem onClick={handleClose}>Profile</MenuItem>
-        <MenuItem onClick={handleClose}>My account</MenuItem>
-        <MenuItem onClick={handleClose}>Logout</MenuItem>
-      </Menu>
-      <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
-        Categories
-      </Button>
-      <Menu
-        id="simple-menu"
-        anchorEl={anchorEl}
-        keepMounted
-        open={Boolean(anchorEl)}
-        onClose={handleClose}
-      >
-        <MenuItem onClick={handleClose}>Profile</MenuItem>
-        <MenuItem onClick={handleClose}>My account</MenuItem>
-        <MenuItem onClick={handleClose}>Logout</MenuItem>
-      </Menu>
-      
-      <Button color="primary">Sign In</Button>
-      <Button color="primary">Get Started</Button> 
-      </div>
+    <div>
+      <Box display="flex" justifyContent="space-between" padding="20px">
+        <AccountBalanceIcon />
 
-    </Box>
+        <div>
+          <Button
+            aria-controls="simple-menu"
+            aria-haspopup="true"
+            onClick={handleClick}
+          >
+            Trend
+          </Button>
+          <Menu
+            id="simple-menu"
+            anchorEl={anchorEl}
+            keepMounted
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+          >
+            <MenuItem onClick={handleClose}>Profile</MenuItem>
+            <MenuItem onClick={handleClose}>My account</MenuItem>
+            <MenuItem onClick={handleClose}>Logout</MenuItem>
+          </Menu>
+          <Button
+            aria-controls="simple-menu"
+            aria-haspopup="true"
+            onClick={handleClick}
+          >
+            Categories
+          </Button>
+          <Menu
+            id="simple-menu"
+            anchorEl={anchorEl}
+            keepMounted
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+          >
+            <MenuItem onClick={handleClose}>Profile</MenuItem>
+            <MenuItem onClick={handleClose}>My account</MenuItem>
+            <MenuItem onClick={handleClose}>Logout</MenuItem>
+          </Menu>
+
+          <Button color="primary">Sign In</Button>
+          <Button color="primary">Get Started</Button>
+        </div>
+      </Box>
+      <Register />
+    </div>
   );
 }

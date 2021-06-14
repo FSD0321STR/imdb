e00a02e2993c2a5ba0c4d132564f41cb7c814d42
Giorgos Desktop <plokamistr@gmail.com>
2021-06-14 10:19:43 +0200

    Fix for icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
-import React, { useState } from 'react'
-import Header from './components/header/header';
-import Footer from './components/footer/footer';
+import React, { useState } from "react";
+import Header from "./components/header/header";
+import Footer from "./components/footer/footer";
 
 function App() {
- 
-  return (
-    <div>
-      <Header/>
-      <Footer/>
-    </div>
-
-  )
+	return (
+		<div>
+			<Header />
+			<Footer />
+		</div>
+	);
 }
 
-export default App
+export default App;

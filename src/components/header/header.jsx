08530208cbccb79195179e588c796c5e6a61de86
Giorgos Desktop <plokamistr@gmail.com>
2021-06-16 12:30:06 +0200

    Collapse register function

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,70 +1,79 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "@material-ui/core/Button";
 import Menu from "@material-ui/core/Menu";
 import MenuItem from "@material-ui/core/MenuItem";
-import { Box, Icon } from "@material-ui/core";
+import { Box } from "@material-ui/core";
 import AccountBalanceIcon from "@material-ui/icons/AccountBalance";
-import Register from "../register/register"
+import Register from "../register/register";
+import Collapse from "@material-ui/core/Collapse";
 
 export default function Header() {
-  const [anchorEl, setAnchorEl] = React.useState(null);
+	const [anchorEl, setAnchorEl] = React.useState(null);
+	const [open, setOpen] = useState(false);
 
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
+	const handleRegisterCollapse = (event) => {
+		setOpen((prev) => !prev);
+	};
 
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
+	const handleClick = (event) => {
+		setAnchorEl(event.currentTarget);
+	};
+	const handleClose = () => {
+		setAnchorEl(null);
+	};
 
-  return (
-    <div>
-      <Box display="flex" justifyContent="space-between" padding="20px">
-        <AccountBalanceIcon />
+	return (
+		<div>
+			<Box display="flex" justifyContent="space-between" padding="20px">
+				<AccountBalanceIcon />
 
-        <div>
-          <Button
-            aria-controls="simple-menu"
-            aria-haspopup="true"
-            onClick={handleClick}
-          >
-            Trend
-          </Button>
-          <Menu
-            id="simple-menu"
-            anchorEl={anchorEl}
-            keepMounted
-            open={Boolean(anchorEl)}
-            onClose={handleClose}
-          >
-            <MenuItem onClick={handleClose}>Profile</MenuItem>
-            <MenuItem onClick={handleClose}>My account</MenuItem>
-            <MenuItem onClick={handleClose}>Logout</MenuItem>
-          </Menu>
-          <Button
-            aria-controls="simple-menu"
-            aria-haspopup="true"
-            onClick={handleClick}
-          >
-            Categories
-          </Button>
-          <Menu
-            id="simple-menu"
-            anchorEl={anchorEl}
-            keepMounted
-            open={Boolean(anchorEl)}
-            onClose={handleClose}
-          >
-            <MenuItem onClick={handleClose}>Profile</MenuItem>
-            <MenuItem onClick={handleClose}>My account</MenuItem>
-            <MenuItem onClick={handleClose}>Logout</MenuItem>
-          </Menu>
+				<div>
+					<Button
+						aria-controls="simple-menu"
+						aria-haspopup="true"
+						onClick={handleClick}
+					>
+						Trend
+					</Button>
+					<Menu
+						id="simple-menu"
+						anchorEl={anchorEl}
+						keepMounted
+						open={Boolean(anchorEl)}
+						onClose={handleClose}
+					>
+						<MenuItem onClick={handleClose}>Profile</MenuItem>
+						<MenuItem onClick={handleClose}>My account</MenuItem>
+						<MenuItem onClick={handleClose}>Logout</MenuItem>
+					</Menu>
+					<Button
+						aria-controls="simple-menu"
+						aria-haspopup="true"
+						onClick={handleClick}
+					>
+						Categories
+					</Button>
+					<Menu
+						id="simple-menu"
+						anchorEl={anchorEl}
+						keepMounted
+						open={Boolean(anchorEl)}
+						onClose={handleClose}
+					>
+						<MenuItem onClick={handleClose}>Profile</MenuItem>
+						<MenuItem onClick={handleClose}>My account</MenuItem>
+						<MenuItem onClick={handleClose}>Logout</MenuItem>
+					</Menu>
 
-          <Button color="primary">Sign In</Button>
-          <Button color="primary">Get Started</Button>
-        </div>
-      </Box>
-      <Register />
-    </div>
-  );
+					<Button color="primary">Sign In</Button>
+					<Button color="primary" onClick={handleRegisterCollapse}>
+						Get Started
+					</Button>
+				</div>
+			</Box>
+			<Collapse in={open}>
+				<Register />
+			</Collapse>
+		</div>
+	);
 }

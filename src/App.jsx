25f62682e3117801230135319913dd13f257ad2c
Mounir <moner977@gmail.com>
2021-06-17 20:34:00 +0200

    added category

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 import Header from "./components/header/header";
 import Footer from "./components/footer/footer";
+import Category from "./components/categories/category" 
 
 function App() {
 	return (
 		<div>
 			<Header />
+			<Category />
 			<Footer />
 		</div>
 	);

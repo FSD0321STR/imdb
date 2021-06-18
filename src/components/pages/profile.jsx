df0f29ad724986d16c63cb84874317b7d6133a68
Giorgos Desktop <plokamistr@gmail.com>
2021-06-18 21:15:40 +0200

    Function to exclude entering Profile if not signed in

--- src/components/pages/profile.jsx
+++ src/components/pages/profile.jsx
@@ -1,12 +1,16 @@
-import React from 'react';
-import Typography from '@material-ui/core/Typography';
-
+import React from "react";
+import useAuth from "../../hooks/useAuth";
+import Typography from "@material-ui/core/Typography";
 
 export default function Profile() {
+	const { email } = useAuth();
 
-    return (
-        <Typography component="h1">
-            Profile
-        </Typography>
-    );
+	return (
+		<div>
+			<Typography component="h1">Profile</Typography>
+			<Typography component="h2">
+				User <strong>{email}</strong> is logged in
+			</Typography>
+		</div>
+	);
 }

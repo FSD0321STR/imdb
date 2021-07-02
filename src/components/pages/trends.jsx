68ea909f1e256f1db441900f665231da4517ce68
Giorgos Desktop <plokamistr@gmail.com>
2021-07-02 20:53:25 +0200

    Trends progress

--- src/components/pages/trends.jsx
+++ src/components/pages/trends.jsx
@@ -1,276 +1,154 @@
 import React from "react";
-import PropTypes from "prop-types";
-import { withStyles } from "@material-ui/core/styles";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import Toolbar from "@material-ui/core/Toolbar";
-import IconButton from "@material-ui/core/IconButton";
-import SearchIcon from "@material-ui/icons/Search";
+import Container from "@material-ui/core/Container";
 import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
 import Grid from "@material-ui/core/Grid";
 import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
-import Hidden from "@material-ui/core/Hidden";
 import Button from "@material-ui/core/Button";
 import Divider from "@material-ui/core/Divider";
-import Markdown from "../Markdown";
-import post1 from "./posts/blog-post.1.md";
-import post2 from "./posts/blog-post.2.md";
-import post3 from "./posts/blog-post.3.md";
+import { makeStyles } from "@material-ui/core";
 
-const styles = (theme) => ({
-	layout: {
-		width: "auto",
-		marginLeft: theme.spacing.unit * 3,
-		marginRight: theme.spacing.unit * 3,
-		[theme.breakpoints.up(1100 + theme.spacing.unit * 3 * 2)]: {
-			width: 1100,
-			marginLeft: "auto",
-			marginRight: "auto",
-		},
-	},
-	toolbarMain: {
-		borderBottom: `1px solid ${theme.palette.grey[300]}`,
-	},
-	toolbarTitle: {
-		flex: 1,
-	},
-	toolbarSecondary: {
-		justifyContent: "space-between",
-	},
+const useStyles = makeStyles(() => ({
 	mainFeaturedPost: {
-		backgroundColor: theme.palette.grey[800],
-		color: theme.palette.common.white,
-		marginBottom: theme.spacing.unit * 4,
+		marginBottom: "5em",
+		padding: "1.3em",
 	},
-	mainFeaturedPostContent: {
-		padding: `${theme.spacing.unit * 6}px`,
-		[theme.breakpoints.up("md")]: {
-			paddingRight: 0,
-		},
-	},
-	mainGrid: {
-		marginTop: theme.spacing.unit * 3,
-	},
-	card: {
-		display: "flex",
-	},
-	cardDetails: {
-		flex: 1,
-	},
-	cardMedia: {
-		width: 160,
-	},
-	markdown: {
-		padding: `${theme.spacing.unit * 3}px 0`,
-	},
-	sidebarAboutBox: {
-		padding: theme.spacing.unit * 2,
-		backgroundColor: theme.palette.grey[200],
-	},
-	sidebarSection: {
-		marginTop: theme.spacing.unit * 3,
-	},
-	footer: {
-		backgroundColor: theme.palette.background.paper,
-		marginTop: theme.spacing.unit * 8,
-		padding: `${theme.spacing.unit * 6}px 0`,
-	},
-});
+	mainFeaturedPostContent: {},
+	card: {},
+	cardDetails: {},
+	cardMedia: {},
+	sidebarAboutBox: {},
+	sidebarSection: {},
+}));
 
-const sections = [
-	"Technology",
-	"Design",
-	"Culture",
-	"Business",
-	"Politics",
-	"Opinion",
-	"Science",
-	"Health",
-	"Style",
-	"Travel",
-];
-
-const featuredPosts = [
+const featuredTopics = [
 	{
-		title: "Featured post",
+		name: "Most Trend Topic Title",
 		date: "Nov 12",
-		description:
-			"This is a wider card with supporting text below as a natural lead-in to additional content.",
-	},
-	{
-		title: "Post title",
-		date: "Nov 11",
-		description:
-			"This is a wider card with supporting text below as a natural lead-in to additional content.",
+		desc: "Multiple lines of text that form the lede, informing new readers quickly and efficiently about what&apos;s most interesting in this post&apos;s contents.",
+		image:
+			"data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22288%22%20height%3D%22225%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20288%20225%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_164edaf95ee%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A14pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_164edaf95ee%22%3E%3Crect%20width%3D%22288%22%20height%3D%22225%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2296.32500076293945%22%20y%3D%22118.8%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E",
 	},
 ];
 
-const posts = [post1, post2, post3];
+const topics = [
+	{
+		name: "Topic 1",
+		date: "Nov 12",
+		desc: "This is a wider card with supporting text below as a natural lead-in to additional content.",
+		image:
+			"data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22288%22%20height%3D%22225%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20288%20225%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_164edaf95ee%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A14pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_164edaf95ee%22%3E%3Crect%20width%3D%22288%22%20height%3D%22225%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2296.32500076293945%22%20y%3D%22118.8%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E",
+	},
+	{
+		name: "Topic 2",
+		date: "Nov 12",
+		desc: "This is a wider card with supporting text below as a natural lead-in to additional content.",
+		image:
+			"data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22288%22%20height%3D%22225%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20288%20225%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_164edaf95ee%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A14pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_164edaf95ee%22%3E%3Crect%20width%3D%22288%22%20height%3D%22225%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2296.32500076293945%22%20y%3D%22118.8%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E",
+	},
+	{
+		name: "Topic 3",
+		date: "Nov 12",
+		desc: "This is a wider card with supporting text below as a natural lead-in to additional content.",
+		image:
+			"data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22288%22%20height%3D%22225%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20288%20225%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_164edaf95ee%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A14pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_164edaf95ee%22%3E%3Crect%20width%3D%22288%22%20height%3D%22225%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2296.32500076293945%22%20y%3D%22118.8%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E",
+	},
+	{
+		name: "Topic 4",
+		date: "Nov 12",
+		desc: "This is a wider card with supporting text below as a natural lead-in to additional content.",
+		image:
+			"data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22288%22%20height%3D%22225%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20288%20225%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_164edaf95ee%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A14pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_164edaf95ee%22%3E%3Crect%20width%3D%22288%22%20height%3D%22225%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2296.32500076293945%22%20y%3D%22118.8%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E",
+	},
+];
 
 const archives = [
 	"March 2020",
 	"February 2020",
 	"January 2020",
-	"December 2019",
-	"November 2019",
-	"October 2019",
-	"September 2019",
-	"August 2019",
-	"July 2019",
-	"June 2019",
-	"May 2019",
-	"April 2019",
+	"November 1999",
+	"October 1999",
+	"September 1999",
+	"August 1999",
+	"July 1999",
+	"June 1999",
+	"May 1999",
+	"April 1999",
 ];
 
-const social = ["GitHub", "Twitter", "Facebook"];
-
-function Blog(props) {
-	const { classes } = props;
+export default function Trends() {
+	const {
+		mainFeaturedPost,
+		mainFeaturedPostContent,
+		card,
+		cardGrid,
+		cardDetails,
+		cardMedia,
+	} = useStyles();
 
 	return (
-		<React.Fragment>
-			<CssBaseline />
-			<div className={classes.layout}>
-				<Toolbar className={classes.toolbarMain}>
-					<Button size="small">Subscribe</Button>
-					<Typography
-						variant="headline"
-						color="inherit"
-						align="center"
-						noWrap
-						className={classes.toolbarTitle}
-					>
-						Blog
-					</Typography>
-					<IconButton>
-						<SearchIcon />
-					</IconButton>
-					<Button variant="outlined" size="small">
-						Sign up
-					</Button>
-				</Toolbar>
-				<Toolbar variant="dense" className={classes.toolbarSecondary}>
-					{sections.map((section) => (
-						<Typography color="inherit" noWrap key={section}>
-							{section}
-						</Typography>
-					))}
-				</Toolbar>
+		<div>
+			<Container>
 				<main>
-					{/* Main featured post */}
-					<Paper className={classes.mainFeaturedPost}>
-						<Grid container>
-							<Grid item md={6}>
-								<div className={classes.mainFeaturedPostContent}>
-									<Typography variant="display2" color="inherit" gutterBottom>
-										Title of a longer featured blog post
-									</Typography>
-									<Typography variant="headline" color="inherit" paragraph>
-										Multiple lines of text that form the lede, informing new
-										readers quickly and efficiently about what&apos;s most
-										interesting in this post&apos;s contents.
-									</Typography>
-									<Typography variant="title" color="inherit">
-										Continue reading...
-									</Typography>
-								</div>
-							</Grid>
+					{/* Main featured Topic */}
+					<Paper className={mainFeaturedPost}>
+						<Grid container spacing={3} xs={12}>
+							{featuredTopics.map((featuredtopic) => (
+								<Grid item key={featuredtopic.name} md={6}>
+									<div className={mainFeaturedPostContent}>
+										<Typography variant="h3" gutterBottom>
+											{featuredtopic.name}
+										</Typography>
+										<Typography paragraph>{featuredtopic.desc}</Typography>
+										<Typography color="inherit">Continue reading...</Typography>
+									</div>
+								</Grid>
+							))}
 						</Grid>
 					</Paper>
-					{/* End main featured post */}
-					{/* Sub featured posts */}
-					<Grid container spacing={40} className={classes.cardGrid}>
-						{featuredPosts.map((post) => (
-							<Grid item key={post.title} xs={12} md={6}>
-								<Card className={classes.card}>
-									<div className={classes.cardDetails}>
+					{/* End Main featured Topic */}
+
+					{/* Start Topics List */}
+					<Grid container className={cardGrid}>
+						{topics.map((topic) => (
+							<Grid item key={topic.name} xs={12} md={6}>
+								<Card className={card}>
+									<CardActionArea>
+										<CardMedia
+											component="img"
+											alt="Contemplative Reptile"
+											height="140"
+											image="/static/images/cards/contemplative-reptile.jpg"
+											title="Contemplative Reptile"
+										/>
 										<CardContent>
-											<Typography variant="headline">{post.title}</Typography>
-											<Typography variant="subheading" color="textSecondary">
-												{post.date}
+											<Typography gutterBottom variant="h5" component="h3">
+												{topic.name}
 											</Typography>
-											<Typography variant="subheading" paragraph>
-												{post.description}
-											</Typography>
-											<Typography variant="subheading" color="primary">
-												Continue reading...
+											<Typography color="textSecondary" component="p">
+												{topic.desc}
 											</Typography>
 										</CardContent>
-									</div>
-									<Hidden xsDown>
-										<CardMedia
-											className={classes.cardMedia}
-											image="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22288%22%20height%3D%22225%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20288%20225%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_164edaf95ee%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A14pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_164edaf95ee%22%3E%3Crect%20width%3D%22288%22%20height%3D%22225%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2296.32500076293945%22%20y%3D%22118.8%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" // eslint-disable-line max-len
-											title="Image title"
-										/>
-									</Hidden>
+									</CardActionArea>
+									<CardActions>
+										<Button size="small" color="primary">
+											Share
+										</Button>
+										<Button size="small" color="primary">
+											Continue reading...
+										</Button>
+									</CardActions>
 								</Card>
 							</Grid>
 						))}
 					</Grid>
-					{/* End sub featured posts */}
-					<Grid container spacing={40} className={classes.mainGrid}>
-						{/* Main content */}
-						<Grid item xs={12} md={8}>
-							<Typography variant="title" gutterBottom>
-								From the Firehose
-							</Typography>
-							<Divider />
-							{posts.map((post) => (
-								<Markdown
-									className={classes.markdown}
-									key={post.substring(0, 40)}
-								>
-									{post}
-								</Markdown>
-							))}
-						</Grid>
-						{/* End main content */}
-						{/* Sidebar */}
-						<Grid item xs={12} md={4}>
-							<Paper elevation={0} className={classes.sidebarAboutBox}>
-								<Typography variant="title" gutterBottom>
-									About
-								</Typography>
-								<Typography>
-									Etiam porta sem malesuada magna mollis euismod. Cras mattis
-									consectetur purus sit amet fermentum. Aenean lacinia bibendum
-									nulla sed consectetur.
-								</Typography>
-							</Paper>
-							<Typography
-								variant="title"
-								gutterBottom
-								className={classes.sidebarSection}
-							>
-								Archives
-							</Typography>
-							{archives.map((archive) => (
-								<Typography key={archive}>{archive}</Typography>
-							))}
-							<Typography
-								variant="title"
-								gutterBottom
-								className={classes.sidebarSection}
-							>
-								Social
-							</Typography>
-							{social.map((network) => (
-								<Typography key={network}>{network}</Typography>
-							))}
-						</Grid>
-						{/* End sidebar */}
-					</Grid>
+					{/* End Topics List */}
 				</main>
-			</div>
-		</React.Fragment>
+			</Container>
+		</div>
 	);
 }
-
-Blog.propTypes = {
-	classes: PropTypes.object.isRequired,
-};
-
-export default withStyles(styles)(Blog);

4a39f4022becab794100a06f642e9d10e8ddd034
Giorgos Desktop <plokamistr@gmail.com>
2021-07-03 12:09:54 +0200

    Home change

--- src/components/search/search.jsx
+++ src/components/search/search.jsx
@@ -1,40 +1,45 @@
-import React from 'react';
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
-import Autocomplete from '@material-ui/lab/Autocomplete';
+import React from "react";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import Autocomplete from "@material-ui/lab/Autocomplete";
 
 export default function Search() {
-  let value = "";
-  const search = () => 
-  {
-    console.log(value);
-    if(value == "" || value == " ") alert('error, empty search input')
-  }
-  const handleChange = (event) => { value = event.target.innerText };
+	let value = "";
+	const search = () => {
+		console.log(value);
+		if (value == "" || value == " ") alert("error, empty search input");
+	};
+	const handleChange = (event) => {
+		value = event.target.innerText;
+	};
 
-  return (
-      <div class="search">
-        <Autocomplete
-            id="combo-box-demo"
-            options={top100Films}
-            getOptionLabel={(option) => option.title}
-            style={{ width: 300 }}
-            renderInput={(params) => <TextField {...params} label="Search..." variant="outlined" />}
-            onChange={handleChange}
-            />
-            <Button variant="contained" color="primary"  onClick={search}> Search</Button>
-    </div> 
-  );
+	return (
+		<div class="search">
+			<Autocomplete
+				id="combo-box-demo"
+				options={top100Films}
+				getOptionLabel={(option) => option.title}
+				style={{ width: 300 }}
+				renderInput={(params) => (
+					<TextField {...params} label="Search..." variant="outlined" />
+				)}
+				onChange={handleChange}
+			/>
+			<Button variant="contained" color="primary" onClick={search}>
+				{" "}
+				Search
+			</Button>
+		</div>
+	);
 }
 
 // Top 100 films as rated by IMDb users. http://www.imdb.com/chart/top
 const top100Films = [
-    { title: 'Mobiles' },
-    { title: 'Portatiles' },
-    { title: 'Sobremesas' },
-    { title: 'Tablets' },
-    { title: 'Drones' },
-    { title: 'Accesorioss' },
-    { title: 'Tech' }
-
-]
+	{ title: "Mobiles" },
+	{ title: "Portatiles" },
+	{ title: "Sobremesas" },
+	{ title: "Tablets" },
+	{ title: "Drones" },
+	{ title: "Accesorioss" },
+	{ title: "Tech" },
+];

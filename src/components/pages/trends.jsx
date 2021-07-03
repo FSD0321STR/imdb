c00e3fb3d167257b2ede05d777a464cc4844cf4c
Jash <jashanmunder>
2021-07-03 20:49:17 +0200

    get trends by topic

--- src/components/pages/trends.jsx
+++ src/components/pages/trends.jsx
@@ -11,6 +11,9 @@ import CardMedia from "@material-ui/core/CardMedia";
 import Button from "@material-ui/core/Button";
 import Divider from "@material-ui/core/Divider";
 import { makeStyles } from "@material-ui/core";
+import { useEffect } from "react";
+import api from "../../utils/auth-api";
+
 
 const useStyles = makeStyles(() => ({
 	mainFeaturedPost: {
@@ -38,7 +41,20 @@ const featuredTopics = [
 	},
 ];
 
-const topics = [
+const topics = [];
+async function getTrendTopics() {
+	return await api
+		.getByCategory({ category: "Tech & Trend" })
+		.then((t) => {
+			localStorage.setItem("Topics", JSON.stringify(t));
+			topics.push(...t);	
+			})
+		.catch((e) => {
+			Promise.reject(e);
+		});
+}
+console.log(topics)	
+/*[
 	{
 		name: "Topic 1",
 		date: "Nov 12",
@@ -70,7 +86,7 @@ const topics = [
 			"data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22288%22%20height%3D%22225%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20288%20225%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_164edaf95ee%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A14pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_164edaf95ee%22%3E%3Crect%20width%3D%22288%22%20height%3D%22225%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2296.32500076293945%22%20y%3D%22118.8%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E",
 		category: "",
 	},
-];
+];*/
 
 const archives = [
 	"March 2020",
@@ -98,6 +114,10 @@ export default function Trends() {
 		sidebarSection,
 		sidebarAboutBox,
 	} = useStyles();
+
+	useEffect(() => {
+		getTrendTopics();
+	  }, []);
 
 	return (
 		<div>
@@ -132,7 +152,7 @@ export default function Trends() {
 					<Grid container className={mainGrid}>
 						<Grid xs={12} md={9} container spacing={4} className={cardGrid}>
 							{topics.map((topic) => (
-								<Grid item key={topic.name}>
+								<Grid item key={topic.title}>
 									<Card className={card}>
 										<CardActionArea>
 											<CardMedia
@@ -144,7 +164,7 @@ export default function Trends() {
 											/>
 											<CardContent>
 												<Typography gutterBottom variant="h5" component="h3">
-													{topic.name}
+													{topic.title}
 												</Typography>
 												<Typography color="textSecondary" component="p">
 													{topic.desc}

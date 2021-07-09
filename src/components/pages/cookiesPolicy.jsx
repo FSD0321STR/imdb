b4a9f82425691fdde436e2915ca2e2e9eb46a978
Mounir <moner977@gmail.com>
2021-07-09 18:49:37 +0200

    Added footer

--- src/components/pages/cookiesPolicy.jsx
+++ src/components/pages/cookiesPolicy.jsx
@@ -5,8 +5,19 @@ import Typography from '@material-ui/core/Typography';
 export default function CookiesPolicy() {
 
     return (
+        <div>
         <Typography component="h1">
             Cookies Policy
         </Typography>
+        
+        <Typography>
+            
+        </Typography>
+        
+        
+        </div>
+
+        
+        
     );
 }
